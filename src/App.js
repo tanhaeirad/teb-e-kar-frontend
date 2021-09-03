dda450fdd1344786012841439818819450ada28f
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-09-03 19:40:47 +0430

    Install eslint and prettier!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
-};
+const App = () => (
+  <div className="app">
+    <h1>Hello, world!</h1>
+  </div>
+);
 
 export default App;
