d338b0f9bf2684e3d43fae889297e3324693cf4d
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-09-05 22:53:21 +0430

    Add purple and white color collection & change container's max-width!

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
       fontFamily: {
         vazir: ['Vazir', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
       },
+      colors: {
+        purple: {
+          primary: '#243e63',
+          secondary: '#6415ff',
+          dark: '#5011cc',
+        },
+        white: {
+          primary: '#edf2f7',
+          secondary: '#f7fafc',
+        },
+      },
+      container: {
+        screens: {
+          sm: '100%',
+          md: '100%',
+          lg: '1024px',
+          xl: '1280px',
+        },
+      },
     },
   },
   variants: {
