15a2d6fc053240d370caaa10801f68fef12f4192
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-09-07 12:00:36 +0430

    Add ServiceList component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Hero from './components/Hero';
+import ServiceList from './components/ServiceList';
 import Navbar from './layouts/Navbar';
 
 const App = () => (
   <div className="bg-white-pure font-vazir min-h-screen p-8">
     <Navbar />
     <Hero />
+    <ServiceList />
   </div>
 );
 
