09bc386a107956ad444eea2be2a527641701b9cd
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-09-08 00:14:49 +0430

    Add BlogList component!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import OurTeam from './components/OurTeam';
 import ServiceList from './components/ServiceList';
 import Navbar from './layouts/Navbar';
+import BlogList from './components/BlogList';
 
 const App = () => (
   <div className="bg-white-pure font-vazir min-h-screen p-8">
@@ -11,6 +12,7 @@ const App = () => (
     <Hero />
     <ServiceList />
     <OurTeam />
+    <BlogList />
   </div>
 );
 
